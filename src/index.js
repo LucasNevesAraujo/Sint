84d4e841fa6bde42af56e9245b063f43c28f57fe
Lucas Neves Araujo <araujo.lucas@gmail.com>
2017-08-29 15:35:40 -0300

    Exporting module

--- src/index.js
+++ src/index.js
@@ -7,4 +7,22 @@ export {
     consts,
 };
 
-global.Sint = exports; // eslint-disable-line
+const Sint = exports; // eslint-disable-line
+
+global.Sint = Sint; // eslint-disable-line
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') // eslint-disable-line
+{
+    module.exports = Sint; // eslint-disable-line
+}
+else if (typeof define === 'function' && define.amd) // eslint-disable-line
+{
+    define([], function () // eslint-disable-line
+    {
+        return Sint;
+    });
+}
+else
+{
+    window.Sint = Sint; // eslint-disable-line
+}
